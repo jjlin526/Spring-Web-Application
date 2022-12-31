81ec02abb7e5ec1f6bb82a1ef60e0a843bc76eb9
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-30 20:57:35 -0500

    Add controller

--- src/main/java/com/oreilly/demo/controllers/HelloController.java
+++ src/main/java/com/oreilly/demo/controllers/HelloController.java
@@ -1,2 +1,19 @@
-package com.oreilly.demo.controllers;public class HelloController {
+package com.oreilly.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// receive HTTP requests and return response
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello") // http:://localhost:8080/hello?name=Dolly --> spring maps to this function
+    public String sayHello(
+            // map param query string to name
+            @RequestParam(value = "name", required = false, defaultValue="World") String name, Model model) {
+        model.addAttribute("user", name); // look for hello.html in src/main/resources/templates
+        return "hello";
+        // spring takes all attributes from model; adds them to http servlet request and forward it to destination
+    }
 }
