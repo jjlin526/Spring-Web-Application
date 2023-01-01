d29266439928598b4e9c09a07a26d3950447ecee
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-31 20:40:15 -0500

    Make Greeting class a bean

--- src/test/java/com/oreilly/demo/DemoApplicationTests.java
+++ src/test/java/com/oreilly/demo/DemoApplicationTests.java
@@ -1,12 +1,16 @@
 package com.oreilly.demo;
 
+import com.oreilly.demo.json.Greeting;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -21,4 +25,9 @@ class DemoApplicationTests {
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 
+    @Test
+    void noGreetingInAppCtx() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> context.getBean(Greeting.class));
+    }
 }
