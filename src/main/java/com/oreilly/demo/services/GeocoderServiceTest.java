841fc444c8ab6499f1bdc98bf97d57117ffa34ff
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-02 08:11:21 -0500

    Create slf4j logger instance and autowire in a GeocoderService object

--- src/main/java/com/oreilly/demo/services/GeocoderServiceTest.java
+++ src/main/java/com/oreilly/demo/services/GeocoderServiceTest.java
@@ -1,4 +1,16 @@
 package com.oreilly.demo.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class GeocoderServiceTest {
+    // many logging APIs: log4j, slf4j, logback, etc.
+    private final Logger logger = LoggerFactory.getLogger(GeocoderServiceTest.class);
+
+    @Autowired
+    private GeocoderService service;
+
 }
