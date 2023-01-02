354c450357fc13da32cb2298d4870a7e863acde2
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-02 09:23:16 -0500

    Add annotation to formatted_address field as a general purpose mechanism (whenever property in the bean does not match the JSON field)

--- src/main/java/com/oreilly/demo/json/Result.java
+++ src/main/java/com/oreilly/demo/json/Result.java
@@ -1,6 +1,10 @@
 package com.oreilly.demo.json;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Result {
+
+    @JsonProperty("formatted_address")
     private String formattedAddress;
     private Geometry geometry;
 
