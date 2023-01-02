e9bbf56a00b328d388391282d7c5048ea3a4610f
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-01 21:44:11 -0500

    Add AstroService test for asynchronous WebClient

--- src/main/java/com/oreilly/demo/services/AstroServiceTest.java
+++ src/main/java/com/oreilly/demo/services/AstroServiceTest.java
@@ -27,4 +27,17 @@ public class AstroServiceTest {
                 () -> assertEquals(number, people.size())
         );
     }
+
+    @Test
+    void getAstronautsWC() {
+        AstroResult result = service.getAstronautsWC();
+        int number = result.getNumber();
+        System.out.println("There are " + number + " people in space");
+        List<Assignment> people = result.getPeople();
+        people.forEach(System.out::println);
+        assertAll(
+                () -> assertTrue(number >= 0),
+                () -> assertEquals(number, people.size())
+        );
+    }
 }
